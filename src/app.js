379ac0eace0377335107cd04bbc9f06fd3e785d8
T1t0-G40n4 <titogaona84@gmail.com>
2025-04-09 18:40:47 +0000

    Ejercicio Finalizado

--- src/app.js
+++ src/app.js
@@ -5,58 +5,60 @@ import "./assets/img/4geeks.ico";
 
 
 let symbols = ["hearts","diamonds","spades","club"];
-let cardNumber = ["A",2,3,4,5,6,7,8,9,10,"J","Q","K"];
+let cardNumber = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
 let upSideSymbol = document.getElementById("upSideSymbol");
 let number = document.getElementById("cardNumber");
 let downSideSymbol = document.getElementById("downSideSymbol");
 
-function pickCard(Array) {
-    
- let randomCard= Math.floor(Math.random(Array.length));
- return randomCard;
+function pickCard(Array1, Array2) {
+  
+  let card = [];
+ card.push(Array1[Math.floor(Math.random()* Array1.length)]);
+ card.push(Array2[Math.floor(Math.random()* Array2.length)]);
+ return card;
 }
 
-function chooseColor() {
-if( randomCardSymbol === "hearts" || randomCardSymbol === "diamonds"){
+function chooseColor(Array) {
+if( Array [0] === "hearts" || Array[0] === "diamonds"){
   upSideSymbol.style.color = "red";
   downSideSymbol.style.color = "red";
+  number.style.color = "red";
  } 
- if(randomCardSymbol === "spades" || randomCardSymbol === "club"){
+ if( Array[0] === "spades" || Array[0] === "club"){
   upSideSymbol.style.color = "black";
   downSideSymbol.style.color = "black";
+  number.style.color = "black";
 }
 }
-function chooseSuit(){
-  if (randomCardSymbol === "hearts") {  
+function chooseSuit(Array){
+  if (Array[0] === "hearts") {  
   upSideSymbol.innerHTML = "♥"
   downSideSymbol.innerHTML = "♥"    
   }
-  if (randomCardSymbol === "diamonds") {  
+  else if (Array[0] === "diamonds") {  
   upSideSymbol.innerHTML = "♦"    
   downSideSymbol.innerHTML = "♦"    
   }
-  if (randomCardSymbol === "spades") {  
+  else if (Array[0] === "spades") {  
   upSideSymbol.innerHTML = "♠"    
   downSideSymbol.innerHTML = "♠"    
   }
-  if (randomCardSymbol === "club") {  
+  else{  
   upSideSymbol.innerHTML = "♣"    
   downSideSymbol.innerHTML = "♣"    
   }
 
 }
-
+function value(Array){
+  number.innerHTML = Array[1];
+}
 function cardGenerator(){
-  let randomCardNumber = pickCard(cardNumber);
-  let randomCardSymbol = pickCard(symbols);
-  number.innerHTML = randomCardNumber;
-  upSideSymbol.innerHTML= randomCardSymbol;
-  downSideSymbol.innerHTML= randomCardSymbol;
-  chooseSuit(randomCardSymbol);
-  chooseColor(randomCardSymbol);
-  document.getElementById("cardNumber").innerHTML = randomCardNumber;
-
-
+  let card = pickCard(symbols, cardNumber);
+  console.log(card);
+  chooseColor(card);
+  chooseSuit(card);
+  value(card);
+  console.log(card);
 }
 window.onload = function() {
 cardGenerator()
